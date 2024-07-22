a512e5ae4caa8547d72fb637088f789b01b8e9d1
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2024-07-22 23:16:10 +0100

    bump pragma version

--- src/PanicSell.sol
+++ src/PanicSell.sol
@@ -1,12 +1,10 @@
-// Uzumaki Naruto
-
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.20;
 
 import {ERC20Permit, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract PanicSell is ERC20Permit {
-    constructor() ERC20Permit("Noruta Uzamuki") ERC20("Noruta Uzamuki", "Noruta") {
+    constructor() ERC20Permit("PANICSELL") ERC20("PANICSELL", "PANICSELL") {
         uint256 amount = 720 * 10 ** 9 * 10 ** 18;
         _mint(msg.sender, amount);
     }
